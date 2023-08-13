c4cafde4c87b6a0e59b04a90a05a5d2fad06eedd
MohdSaad <saadyesican@gmail.com>
2023-08-13 18:18:20 +0530

    my first commit with basic structure of website

---
+++ script.js
@@ -0,0 +1,18 @@
+let svg1 = document.querySelector(".first");
+let svg2 = document.querySelector(".second");
+let icon = document.querySelector(".icon");
+let x = window.matchMedia("(min-width: 550px)");
+let circle = document.querySelector(".circle");
+function myfun(x) {
+  if (x.matches) {
+    svg1.classList.add("hidden");
+    svg2.classList.remove("hidden");
+  } else {
+    svg2.classList.add("hidden");
+    svg1.classList.remove("hidden");
+  }
+}
+x.addListener(myfun);
+circle.addEventListener("click", () => {
+  circle.style.background = "red";
+});
